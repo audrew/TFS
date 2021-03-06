b48b0bad70c659bb393fe3b0c5a34b3041b4819a
audrew <audrew31@gmail.com>
2021-03-07 01:14:22 +0200

    server back end
    post delete get

--- routes/api/Items.js
+++ routes/api/Items.js
@@ -4,10 +4,30 @@ const router = express.Router();
 // items model 
 const Item = require('../../models/Item');
 
+//get request
+
 router.get ('/', (req, res) => {
     Item.find()
     .sort({ date: -1 })
     .then(items => res.json(items));
+});
+
+// post request
+
+router.post ('/', (req, res) => {
+ const newItem = new Item({
+     name: req.body.name
+ });
+
+ newItem.save().then(item => res.json(item));
+});
+
+// delete request
+
+router.delete ('/:id', (req, res) => {
+Item.findById(req.params.id)
+.then(item => item.remove().then(() => res.json({success: true})))
+.catch(err => res.status(404).json({success: false}))
 });
 
 module.exports = router;
