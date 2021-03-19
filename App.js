3e356722d7141d91c7618efda24536aa44fbbe65
audrew <audrew31@gmail.com>
2021-03-19 23:01:34 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import Header from './components/header';
+import Header from './components/Header';
 
 const App = () => {
      return  (
